e831de5a6fdb183cc762e58e4e9cd019ad884f28
sergswan <s.lebed8920@icloud.com>
2025-10-22 23:36:34 +0300

    Добавлены библиотеки

---
+++ TravelSchedule/APIKey.swift
@@ -0,0 +1,11 @@
+//
+//  APIKey.swift
+//  TravelSchedule
+//
+//  Created by Сергей Лебедь on 22.10.2025.
+//
+
+import Foundation
+enum APIKey {
+  static let apiKey = "af3eb799-0ceb-4b27-af6f-a5f71b0e4670"
+}
