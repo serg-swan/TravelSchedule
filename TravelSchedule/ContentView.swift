b37de436dd8ded6335c9f5e2ea5bbcccc5e2358c
sergswan <s.lebed8920@icloud.com>
2025-10-25 20:17:25 +0300

    Первый запрос выполнен

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,39 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+                  
+                   testFetchStations()
+               }
+    }
+}
+// Функция для тестового вызова API
+func testFetchStations() {
+    Task {
+        do {
+            let client = Client(
+               
+                serverURL: try Servers.Server1.url(),
+               
+                transport: URLSessionTransport()
+            )
+            
+            let service = NearestStationsService(
+                client: client,
+                apikey: APIKey.apiKey
+            )
+            print("Fetching stations...")
+            let stations = try await service.getNearestStations(
+                lat: 59.864177, // Пример координат
+                lng: 30.319163, // Пример координат
+                distance: 50    // Пример дистанции
+            )
+            print("Successfully fetched stations: \(stations)")
+        } catch {
+          
+            print("Error fetching stations: \(error)")
+          
+        }
     }
 }
 
